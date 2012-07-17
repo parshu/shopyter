3f1078f4b16ee74af15b12b71d0a1dc009ce6ef5
parshu <parshu@simplyhired.com>
2012-07-16 20:58:08 -0700

    Added tabs for saved deals

--- bootstrap/compile.sh
+++ bootstrap/compile.sh
@@ -8,7 +8,8 @@ echo "Done customizing bootstrap-responsive.css ..."
 echo "Customizing bootstrap.css ..."
 sed '194 s/.*/   margin-left: 5px;/' docs/assets/css/bootstrap.css > test1
 sed '319 s/.*/   margin-left: 5px;/' test1 > test2
-sed '409 s/.*/   padding-left: 70px;/' test2 > test3
+sed '3208 s/.*/   margin-bottom: 0px;/' test2 > test21
+sed '409 s/.*/   padding-left: 70px;/' test21 > test3
 sed '2493 s/.*/   padding: 0px;/' test3 > test4
 sed '2496 s/.*/   border: 0px solid #eee;/' test4 > test5
 sed '2497 s/.*/   border: 0px solid rgba(0, 0, 0, 0.05);/' test5 > test6
