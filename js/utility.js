cab4d66c113c3b37cd99d320f344a7368fa7a8e3
parshu <parshu@simplyhired.com>
2012-08-07 23:23:20 -0700

    - fixed maps centering wonkiness - added image to map view

---
+++ js/utility.js
@@ -0,0 +1,24 @@
+function execURL(url, asynchronous){
+		var ajaxRequest;  // The variable that makes Ajax possible!
+		try{
+			// Opera 8.0+, Firefox, Safari
+			ajaxRequest = new XMLHttpRequest();
+		} catch (e){
+			// Internet Explorer Browsers
+			try{
+				ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch (e) {
+				try{
+					ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+				} catch (e){
+					// Something went wrong
+					alert("Your browser broke!");
+					return false;
+				}
+			}
+		}
+		
+		ajaxRequest.open("GET", url, asynchronous); 
+		ajaxRequest.send(null); 
+		
+	}
